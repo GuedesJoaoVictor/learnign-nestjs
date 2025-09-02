5f0795adc06ca0d58577bfc6d944fc755423bef8
Aluno <Aluno@politecnico.local>
2025-09-02 16:54:46 -0300

    feat - brincando com o login

---
+++ src/controller/login.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post } from "@nestjs/common";
+import { PrismaService } from "src/database/prisma.service";
+import { UserDTO } from "src/dtos/user.dto";
+import { LoginService } from "src/service/login.service";
+
+@Controller('login')
+export class LoginController {
+    constructor (private loginService: LoginService) {}
+
+    @Post()
+    login(@Body() body: UserDTO) {
+        return this.loginService.authenticate(body);
+    }
+}
---
+++ src/service/login.service.ts
@@ -0,0 +1,27 @@
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
+import { PrismaService } from "src/database/prisma.service";
+import { UserDTO } from "src/dtos/user.dto";
+
+@Injectable()
+export class LoginService {
+
+    constructor (private prismaService: PrismaService) {}
+
+    async authenticate(body: UserDTO) {
+        const { email, password } = body;
+
+        if (!email || !password) {
+            throw new BadRequestException("É preciso enviar email e senha.");
+        }
+
+        const user = await this.prismaService.user.findUnique({
+            where: { email }
+        });
+
+        if (!user) {
+            throw new NotFoundException("Usuário não encontrado.");
+        }
+
+    }
+
+}
